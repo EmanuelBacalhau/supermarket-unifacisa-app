6c3c39ae4c6a8bd7c76f4fe8539ff233e05c9ca1
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-11-15 18:31:33 -0300

    feat: finish project

--- src/pages/app/Historic/index.tsx
+++ src/pages/app/Historic/index.tsx
@@ -8,9 +8,12 @@ import { OrderDto } from '../../../dtos/OrderDto'
 import { Order } from './components/Order'
 import { useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProp } from '../../../routes/app-routes'
+import { ClockCounterClockwise } from 'phosphor-react-native'
 
 const StyledView = styled(View)
+const StyledText = styled(Text)
 const StyledFlatList = styled(FlatList<OrderDto>)
+const StyledClockCounterClockwise = styled(ClockCounterClockwise)
 
 export function Historic() {
   const { user } = useAuth()
@@ -36,18 +39,29 @@ export function Historic() {
     <StyledView className="flex-1">
       <Header />
 
-      <StyledFlatList
-        data={orders}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item, index }) => (
-          <Order
-            onPress={() => handleGoDetailsOrder(item.id, index)}
-            index={index}
-            item={item}
-          />
-        )}
-        className="px-4 my-2"
-      />
+      {orders.length === 0 ? (
+        <StyledView className="items-center justify-center flex-1">
+          <StyledView className="items-center">
+            <StyledClockCounterClockwise size={50} className="text-gray-500" />
+            <StyledText className="text-lg text-gray-500">
+              Yous shopping cart is empty
+            </StyledText>
+          </StyledView>
+        </StyledView>
+      ) : (
+        <StyledFlatList
+          data={orders}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item, index }) => (
+            <Order
+              onPress={() => handleGoDetailsOrder(item.id, index)}
+              index={index}
+              item={item}
+            />
+          )}
+          className="px-4 my-2"
+        />
+      )}
     </StyledView>
   )
 }
