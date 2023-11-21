f6dd8048eb6bbb6f52cc3e0b0be093654b178f9f
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-11-21 15:42:45 -0300

    fix: finish

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { AppError } from '../utils/AppError'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.121:3333/api',
+  baseURL: 'http:/<ip>:3333/api',
 })
 
 api.interceptors.response.use(
